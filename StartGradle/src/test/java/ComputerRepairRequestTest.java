846f56875f9322e365e02a5a3df5005a9e552a09
teograma <teograma4@gmail.com>
2025-03-03 11:16:03 +0200

    LAB1

---
+++ StartGradle/src/test/java/ComputerRepairRequestTest.java
@@ -0,0 +1,24 @@
+import ro.mpp2024.model.ComputerRepairRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ComputerRepairRequestTest {
+    @Test
+    @DisplayName("First test:")
+
+    public void testExample1() {
+        ComputerRepairRequest request = new ComputerRepairRequest();
+        assertEquals("", request.getOwnerName());
+        assertEquals("", request.getOwnerAddress());
+    }
+
+    @Test
+    @DisplayName("Second test:")
+
+    public void testExample2() {
+        assertEquals(2, 2, "The numbers must be equal!");
+    }
+}
